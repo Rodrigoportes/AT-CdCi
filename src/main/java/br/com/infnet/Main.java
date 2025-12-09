01903f1cb5cdfa2cf410a604d77dc813cec2f6f2
Rodrigo Portes <Rodrigo.r.portes@gmail.com>
2025-12-09 04:19:46 -0300

    Primeiro commit

---
+++ src/main/java/br/com/infnet/Main.java
@@ -0,0 +1,8 @@
+package br.com.infnet;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Status: Aplicação de demonstração do AT-CdCi rodando com sucesso!");
+    }
+}
